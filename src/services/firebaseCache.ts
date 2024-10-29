914e7f0d7363f74afbabf658a21ca8ce8f211e4b
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-10-29 09:13:53 +0900

    firebaseCache.tsの編集

--- src/services/firebaseCache.ts
+++ src/services/firebaseCache.ts
@@ -1,13 +1,13 @@
 import functions from 'firebase-functions';
 import admin from 'firebase-admin';
+import { fetchTasks } from '@/lib/firebase/firebaseStoreFunctions';
 
 admin.initializeApp();
 
 export const getFirestoreData = functions.https.onRequest(async (req, res) => {
     try {
-        const snapshot = await admin.firestore().collection('your_collection').get();
-        const data = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-  
+        const data = await fetchTasks()
+
         res.set('Cache-Control', 'public, max-age=43200, s-maxage=86400');
         res.json(data);
     } catch {
