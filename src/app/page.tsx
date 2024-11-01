cb9859463e6603287f6d59a4c5b87817bbac6287
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-11-01 11:53:09 +0900

    page.tsxの修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 'use client'
 
-import { redirect } from "next/navigation"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react";
 
 const Page = () => {
-    redirect("/login")
+    const router = useRouter();
+
+  useEffect(() => {
+    router.push('/login');
+  }, [router]);
 }
 
 export default Page
