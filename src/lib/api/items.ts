0717ea4f714413a3828b60a32434883488b7c06d
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-12-11 17:30:15 +0900

    リストの検索条件の変更

--- src/lib/api/items.ts
+++ src/lib/api/items.ts
@@ -3,26 +3,20 @@ import { listItemType, postItemType } from "@/types/firebaseDocTypes";
 type fetchItemsPropsType = {
     startDate: string;
     endDate: string;
-    userEmail: string;
+    searchEmail: string;
+    taskNumbers: number[];
 }
-
-type fetchAllUserItemsPropsType = {
-    startDate: string;
-    endDate: string;
+type Condition = | {
+    column: string;
+    operator: string;
+    value: string | number[];
 }
-
-type fetchTaskFilterdItemsPropsType = {
-    startDate: string;
-    endDate: string;
-    userEmail: string;
-    task: string;
+| {
+    $or: Condition[];
 }
-
-type fetchAllUserTaskFilterdItemsPropsType = {
-    startDate: string;
-    endDate: string;
-    task: string;
-}
+| {
+    $and: Condition[];
+};
 
 const token = process.env.NEXT_PUBLIC_API_TOKEN;
 const table_name = "firebase_tasks"
@@ -61,178 +55,46 @@ export const addItem = async(newTask: postItemType) => {
     }
 }
 
-export const fetchItems = async ({startDate, endDate, userEmail}: fetchItemsPropsType) => {
+export const fetchItems = async ({startDate, endDate, searchEmail, taskNumbers }: fetchItemsPropsType) => {
     const url = "https://logiapp.rextlab.com/api/fetch-data"
 
-    const payload = {
-        table_name,
-        "conditions": [
-            {
-                "column": "User",
-                "operator": "=",
-                "value": userEmail
-            },
-            {
-                "column": "DateTimeNum",
-                "operator": "<=",
-                "value": `${endDate.replaceAll("-", "")}2359`
-            },
-            {
-                "column": "DateTimeNum",
-                "operator": ">=",
-                "value": `${startDate.replaceAll("-", "")}0000`
-            },
-        ],
-    };
+    const conditions: Condition[] = [
+        {
+            "column": "DateTimeNum",
+            "operator": "<=",
+            "value": `${endDate.replaceAll("-", "")}2359`
+        },
+        {
+            "column": "DateTimeNum",
+            "operator": ">=",
+            "value": `${startDate.replaceAll("-", "")}0000`
+        },
+    ]
 
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                "Authorization": `Bearer ${token}`,
-            },
-            body: JSON.stringify(payload),
-            mode: 'cors'
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const responseData = await response.json();
-        const fetchData: listItemType[] = responseData.data
-        const sortData = fetchData.sort((a: listItemType, b: listItemType) => Number(a.DateTimeNum) - Number(b.DateTimeNum));
-        return sortData;
-    } catch (error) {
-        console.error("Error occurred while adding item:", error);
-        throw error;
+    if(searchEmail !== "ALL"){
+        conditions.push({
+            "column": "User",
+            "operator": "=",
+            "value": searchEmail
+        })
     }
-}
 
-export const fetchAllUserItems = async ({startDate, endDate}: fetchAllUserItemsPropsType) => {
-    const url = "https://logiapp.rextlab.com/api/fetch-data"
-
-    const payload = {
-        table_name,
-        "conditions": [
-            {
-                "column": "DateTimeNum",
-                "operator": "<=",
-                "value": `${endDate.replaceAll("-", "")}2359`
-            },
-            {
-                "column": "DateTimeNum",
-                "operator": ">=",
-                "value": `${startDate.replaceAll("-", "")}0000`
-            },
-        ],
-    };
-
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                "Authorization": `Bearer ${token}`,
-            },
-            body: JSON.stringify(payload),
-            mode: 'cors'
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const responseData = await response.json();
-        const fetchData: listItemType[] = responseData.data
-        const sortData = fetchData.sort((a: listItemType, b: listItemType) => Number(a.DateTimeNum) - Number(b.DateTimeNum));
-        return sortData;
-    } catch (error) {
-        console.error("Error occurred while adding item:", error);
-        throw error;
+    /*
+    if(taskNumbers.length){
+        conditions.push({
+            "$or": taskNumbers.map(taskNum => ({
+                column: "task",
+                operator: "=",
+                value: taskNum
+            }))
+        })
     }
-}
-
-export const fetchTaskFilterdItems = async ({startDate, endDate, userEmail, task}: fetchTaskFilterdItemsPropsType) => {
-    const url = "https://logiapp.rextlab.com/api/fetch-data"
-
+    */
+    console.log(taskNumbers)
+    console.log(conditions)
     const payload = {
         table_name,
-        "conditions": [
-            {
-                "column": "User",
-                "operator": "=",
-                "value": userEmail
-            },
-            {
-                "column": "DateTimeNum",
-                "operator": "<=",
-                "value": `${endDate.replaceAll("-", "")}2359`
-            },
-            {
-                "column": "DateTimeNum",
-                "operator": ">=",
-                "value": `${startDate.replaceAll("-", "")}0000`
-            },
-            {
-                "column": "task",
-                "operator": "=",
-                "value": task
-            },
-        ],
-    };
-
-    try {
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json",
-                "Authorization": `Bearer ${token}`,
-            },
-            body: JSON.stringify(payload),
-            mode: 'cors'
-        });
-
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-
-        const responseData = await response.json();
-        const fetchData: listItemType[] = responseData.data
-        const sortData = fetchData.sort((a: listItemType, b: listItemType) => Number(a.DateTimeNum) - Number(b.DateTimeNum));
-        return sortData;
-    } catch (error) {
-        console.error("Error occurred while adding item:", error);
-        throw error;
-    }
-}
-
-export const fetchAllUserTaskFilterdItems = async ({startDate, endDate, task}: fetchAllUserTaskFilterdItemsPropsType) => {
-    const url = "https://logiapp.rextlab.com/api/fetch-data"
-
-    const payload = {
-        table_name,
-        "conditions": [
-            {
-                "column": "DateTimeNum",
-                "operator": "<=",
-                "value": `${endDate.replaceAll("-", "")}2359`
-            },
-            {
-                "column": "DateTimeNum",
-                "operator": ">=",
-                "value": `${startDate.replaceAll("-", "")}0000`
-            },
-            {
-                "column": "task",
-                "operator": "=",
-                "value": task
-            },
-        ],
+        "conditions": conditions,
     };
 
     try {
