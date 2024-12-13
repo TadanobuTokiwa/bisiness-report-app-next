32d1bbb1dd7f10c6e92e8613a41676c039e0c7d5
TadanobuTokiwa <tokiwa_tadanobu@rext.work>
2024-12-13 15:26:47 +0900

    CSVデータの抽出項目の変更

--- src/lib/CSVdownloader.ts
+++ src/lib/CSVdownloader.ts
@@ -9,11 +9,13 @@ type propsType = {
 export const downloadCSV = ({data, allTaskItems, filename}: propsType) => {
     if (!data || !data.length) return;
 
-    const headers = ["日付", "業務項目", "開始時間", "終了時間", "業務時間", "件数", "時速", "メールアドレス"];
+    const headers = ["日付", "業務項目", "開始時間", "終了時間", "業務時間", "件数", "時速", "従業員名", "メールアドレス", "業務項目種別", "チームの業務"];
 
     const rows = data.map((item) => {
         const taskItem = allTaskItems?.filter(taskItem => taskItem.id === item.task)
         const taskName = taskItem.length === 1 ? taskItem[0].taskName ? taskItem[0].taskName : "" : ""
+        const taskTeam = taskItem.length === 1 ? taskItem[0].teamName ? taskItem[0].teamName : "" : ""
+        const taskType = taskItem.length === 1 ? taskItem[0].taskType ? taskItem[0].taskType : "" : ""
         return ([
             item.createDate,
             taskName,
@@ -22,7 +24,10 @@ export const downloadCSV = ({data, allTaskItems, filename}: propsType) => {
             item.workingHour.toFixed(3),
             item.kensu,
             item.perHour,
+            item.UserName,
             item.User,
+            taskType,
+            taskTeam
         ])
     });
 
